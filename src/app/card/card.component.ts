b95524086f16262c42b195cebd73a28fe4c2ecc6
Mohmmad Iqbal Hossain Emon <48848070+iqbal-emon@users.noreply.github.com>
2025-01-29 00:35:52 +0600

    ngx-loader added

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,16 +2,23 @@ import { ProductsService } from './../products/products.service';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Env } from '../../environments/env';
+import { NgxSkeletonLoaderModule } from 'ngx-skeleton-loader';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
-  imports: [RouterLink],
+  imports: [RouterLink, NgxSkeletonLoaderModule],
 })
 export class CardComponent {
   constructor(private productService: ProductsService) {}
   @Input() product: any;
+  isLoading: boolean = true;
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
+  }
   getImageUrl(localPath: string): string {
     return this.productService.getImageUrl(localPath);
   }
